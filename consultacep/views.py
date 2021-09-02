48f38f15a2cab48ef3551ef1ebe3344983080492
Olavo-DIO <olavo.melo.br.DIO@gmail.com>
2021-09-02 17:40:32 -0300

    Segundo Commit APP CEP Versão 0.00

--- consultacep/views.py
+++ consultacep/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
+import requests
 
 def consultacep(request):
+
+    dados_html = {}
+    dados_cep = {}
+
     if request.POST:
         cep = request.POST.get('cep')
+        url_cep = "https://viacep.com.br/ws/{}/json".format(cep)
+        response = requests.get(url_cep)
+        status_code = response.status_code
+        dados_cep = response.json()
+
+    return render(request,'consultacep.html', dados_cep)
 
 
-        return render(request,'consultacep.html')
